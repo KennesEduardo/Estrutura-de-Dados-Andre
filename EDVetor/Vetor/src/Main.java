ce623f5acf7b690f863cb3be4039d25a88a7d672
ALUNO <aluno@pedag.local>
2022-03-10 22:30:04 -0300

    comite 3

--- EDVetor/Vetor/src/Main.java
+++ EDVetor/Vetor/src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) throws Exception {
+    try {
         Vetor vetor = new Vetor(5);
-        String Nome = "Kennes";
+        vetor.adiciona("elemeto 1");
+        vetor.adiciona("elemeto 2");
+        vetor.adiciona("elemento 3");
         System.out.println(vetor);
+    	}catch(Exception e){
+    		e.printStackTrace();
+    	}
+    	
     }
-}
+}    
--- EDVetor/Vetor/src/Vetor.java
+++ EDVetor/Vetor/src/Vetor.java
@@ -2,17 +2,49 @@ import java.util.Arrays;
 
 public class Vetor {
     private String[] elementos; //criando vetor privado s√≥ para a classe ter acesso
-
+    private int tamanho;
+    
     public Vetor(int capacidade){//metado construtor 
         elementos = new String[capacidade];
+        tamanho = 0;
     }
 
-	@Override
-	public String toString() {
-		return "Vetor [elementos=" + Arrays.toString(elementos) + "]";
+	
+	/*public void adiciona(String elemento) {
+		for(int i=0; i<elementos.length; i++) {
+			if(elementos[i] ==null) {
+				elementos[i] = elemento;
+				break;
+			}
+		}
 	}
-
-
+	*/
+    public void adiciona(String elemento) throws Exception {
+    	if(tamanho < elementos.length) {
+    		elementos[tamanho] = elemento;
+    		tamanho++;
+    	}else {
+    		throw new Exception("vetor est· cheio");    	
+    		}
+    	}
+    
+	
+	public String toString(){
+		StringBuffer s = new StringBuffer();
+		s.append("[");
+		
+		for(int i = 0; i < tamanho-1; i++) {
+			s.append(this.elementos[i]);
+			s.append(",");
+		}
+		if(tamanho > 0) {
+			s.append(elementos[tamanho-1]);
+		}
+		s.append("]");
+		return s.toString();
+		
+	}
+	
     
 
 }
