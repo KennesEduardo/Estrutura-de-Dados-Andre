a7ed72d71ab481efb022918d2df2a6c1f1a10c28
ALUNO <aluno@pedag.local>
2022-03-10 21:14:13 -0300

    comite 2

--- EDVetor/Vetor/src/Vetor.java
+++ EDVetor/Vetor/src/Vetor.java
@@ -1,9 +1,19 @@
+import java.util.Arrays;
+
 public class Vetor {
     private String[] elementos; //criando vetor privado só para a classe ter acesso
 
     public Vetor(int capacidade){//metado construtor 
         elementos = new String[capacidade];
     }
+
+	@Override
+	public String toString() {
+		return "Vetor [elementos=" + Arrays.toString(elementos) + "]";
+	}
+
+
+    
 
 }
 
