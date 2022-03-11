34f6d6d6a54def15a0a0a801b82eed0337bafedf
ALUNO <aluno@pedag.local>
2022-03-10 21:04:16 -0300

    comite 1

---
+++ EDVetor/Vetor/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Vetor vetor = new Vetor(5);
+        String Nome = "Kennes";
+        System.out.println(vetor);
+    }
+}
---
+++ EDVetor/Vetor/src/Vetor.java
@@ -0,0 +1,10 @@
+public class Vetor {
+    private String[] elementos; //criando vetor privado só para a classe ter acesso
+
+    public Vetor(int capacidade){//metado construtor 
+        elementos = new String[capacidade];
+    }
+
+}
+
+
